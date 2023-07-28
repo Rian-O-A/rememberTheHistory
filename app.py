e2f3b7ecff54f657cd00ec8a3ee47ef95000f8dc
Rian Almeida <rianoliveira38@gmail.com>
2023-07-28 14:20:02 -0300

    create app run

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from backend.auth.route import authDefault
 
 
 app = Flask(__name__)
-
-
-
 app.register_blueprint(authDefault, url_prefix="")
+
+app.run(host="0.0.0.0", port=2000, debug=False)
     
  
