b3680904970ce5d87ae483287d034755ed7b6171
Rian Almeida <rianoliveira38@gmail.com>
2023-07-28 14:30:48 -0300

    add cors ao code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from backend.auth.route import authDefault
-
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
 app.register_blueprint(authDefault, url_prefix="")
+
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.run(host="0.0.0.0", port=2000, debug=False)
     
